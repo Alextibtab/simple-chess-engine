5bcead953554a23eedc049740a96bebf4354a381
Alextibtab <alextabitha27@gmail.com>
2023-07-04 01:03:03 +0100

    Moved GUI chess classes into separate folder. Created basic chess board widget for displaying the board. Added settings button. Created custom settings for tweaking chess things in the future. Added settings to the screenmanager. Added more assets Updated kvlang file to import chess widget so the board is displayed. Added settings/config files as json and ini

--- gui/main.py
+++ gui/main.py
@@ -1,5 +1,8 @@
+import cProfile
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.settings import SettingsWithTabbedPanel
 
 
 class MenuScreen(Screen):
@@ -19,14 +22,44 @@ class ComputerVsComputerScreen(Screen):
 
 
 class ChessApp(App):
-    def build(self):
-        sm = ScreenManager()
-        sm.add_widget(MenuScreen(name="menu"))
-        sm.add_widget(PlayerVsPlayerScreen(name="player_vs_player"))
-        sm.add_widget(PlayerVsComputerScreen(name="player_vs_ai"))
-        sm.add_widget(ComputerVsComputerScreen(name="ai_vs_ai"))
+    def on_start(self):
+        self.profile = cProfile.Profile()
+        self.profile.enable()
 
-        return sm
+    def on_stop(self):
+        self.profile.disable()
+        self.profile.dump_stats("chess_app.profile")
+
+    def build(self):
+        self.settings_cls = SettingsWithTabbedPanel
+        self.icon = "assets/W_King.png"
+
+        self.sm = ScreenManager()
+        self.sm.add_widget(MenuScreen(name="menu"))
+        self.sm.add_widget(PlayerVsPlayerScreen(name="player_vs_player"))
+        self.sm.add_widget(PlayerVsComputerScreen(name="player_vs_ai"))
+        self.sm.add_widget(ComputerVsComputerScreen(name="ai_vs_ai"))
+
+        return self.sm
+
+    def build_config(self, config):
+        config.read("config.ini")
+
+    def display_settings(self, settings):
+        if not self.sm.has_screen("settings"):
+            s = Screen(name="settings")
+            s.add_widget(settings)
+            self.sm.add_widget(s)
+        self.sm.transition.direction = "left"
+        self.sm.current = "settings"
+
+    def build_settings(self, settings):
+        settings.add_json_panel("Chess", self.config, "settings.json")
+
+    def close_settings(self, *largs):
+        if self.sm.current == "settings":
+            self.sm.transition.direction = "right"
+            self.sm.current = "menu"
 
 
 if __name__ == "__main__":
